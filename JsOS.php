9b2132374d0724766536c64c3345616ac38b24f9
HarisonP <harito92@abv.bg>
2013-07-11 12:54:52 +0300

    text push

--- JsOS.php
+++ JsOS.php
@@ -9,24 +9,57 @@
 ?>
 <html>
 	<head>
-		<script src="jquery-1.4.4.min.js"></script>
+		<script src="http://code.jquery.com/jquery-2.0.1.min.js"></script>
 		<script src="resources/window.js"></script>
 		<script src="resources/desktop.js"></script>
 		<link rel="stylesheet" type="text/css" href="resources/defWindowStyle.css">
+		<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+		<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/themes/base/jquery-ui.css"/>
 	</head>
-	<body>
+	<body onload="init()">
 		<div id="desktop">
-
-
-
+			<div id="startMenu"></div>
 			<div id="toolbar">
+				<div id="startBtn">
+					Start
+				</div>
+				<div class="seprtr">
+				</div>
+				<div id="taskbar">
+					<div class="seprtr">
+					</div>
+					<div id="datetime">
+						00:01 | 11.7.2013
+					</div>
+				<div>
 			</div>
 		</div>
+
+
 		<script>
 			$("document").ready(function(){
 				LoadSettings();
 			})
+			function init(){
+				var win = CreateWindow({
+					width:500,
+					height:300,
+					posx:200,
+					posy:200,
+					resizable:true,
+					title:"This is a test window"});
+				AddWindow(win);
+				var win1 = CreateWindow({
+					width:500,
+					height:300,
+					posx:200,
+					posy:200,
+					resizable:true,
+					title:"This is a test window"});
+				AddWindow(win1);
+			}
 		</script>
+
 	</body>
 
 
--- Market/programs/readME.txt
+++ Market/programs/readME.txt
@@ -1,1 +1,1 @@
-This is the place ,where the programs are stored.
+This is the place ,where the programs are stored. HUUUUUUUUUUUUUUUUI
