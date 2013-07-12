ce85e2f64298554f4ce2d01b70dad597a9399285
Mihail Gospodinov <bladespinner@gmail.com>
2013-07-13 02:28:57 +0300

    Добавен Task Меринджей

--- Market/programs/Task Merinjei/main.js
+++ Market/programs/Task Merinjei/main.js
@@ -6,22 +6,43 @@ var _window = CreateWindow({
 	iconUrl:path+"/task.png",
 	resizable:true,
 	title:"Task Merinjei"});
-CreateProcess(_window,"asdf");
+CreateProcess(_window,"taskManager");
 
 var content = _window.content;
 
-function TrackProcesses
+function TrackProcesses()
 {
-	
-}
-
-for(var v in Processes)
-{
-	if(v != "nextId" && v!="GenId")
+	$(content).html("");
+	for(var v in Processes)
 	{
-		$(content).append(v.name+" ");
+		if(v != "nextId" && v!="GenId")
+		{
+			var process = document.createElement("div");
+			var terminateBtn = document.createElement("div");
+			$(process).html(v+"|"+Processes[v].name);
+			terminateBtn.toKill = v;
+			$(process).css("width","95%");
+			$(process).css("height","1.25em");
+			$(process).css("margin-top","2px");
+			$(process).css("margin-left","2px");
+			$(process).css("border","black solid 1px");
+			$(process).css("background","LightGray");
+			$(terminateBtn).css("float","right");
+			$(terminateBtn).css("color","red");
+			$(terminateBtn).css("margin-right","2px");
+			$(terminateBtn).append("X");
+			$(terminateBtn).click(function(){
+				var id = this.toKill;
+				StopProcess(id);
+			});
+			$(process).append(terminateBtn);
+			$(content).append(process);
+		}
 	}
 }
+setInterval(TrackProcesses,1000);
+
+
 /*
 alert("haha");
 $(Processes).on("processAdd",function(e)
