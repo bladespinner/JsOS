8c9267f8bdd0e57506bf36cd1ca46c91fbd7878d
HarisonP <harito92@abv.bg>
2013-07-12 18:28:08 +0300

    working database

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-	define("_APPROOT","/JsOS");	 				#Application root
-	define("_USERFILESYS","/JsOS/fileSystem");
+	define("_APPROOT","/JsOS-master");	 				#Application root
+	define("_USERFILESYS","/JsOS-master/fileSystem");
 	define("_MYSQLSERVER","localhost:3306"); 	#MySQL server location
 	define("_MYSQLUSER","root"); 				#MySQL user
 	define("_MYSQLPASSWORD",""); 				#password for the user
-	define("_MYSQLDATABASE","jsosdb"); 			#database name
+	define("_MYSQLDATABASE","jsosdb"); 
 ?>
