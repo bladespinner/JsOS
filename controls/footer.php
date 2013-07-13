3123b7b86962d4616a3b68ad855b51743b2cf94e
HarisonP <harito92@abv.bg>
2013-07-13 03:47:10 +0300

    fixing footer

--- controls/footer.php
+++ controls/footer.php
@@ -1,6 +1,5 @@
-<?php
-	define("_APPROOT","D:\Psevdodesktop\Hari2\PHP\wamp\www\JsOS-master");	 				#Application root 
-include_once _APPROOT.'/helpers/membership.php';
+<?php	 				#Application root 
+include_once '/helpers/membership.php';
 ?>
 <footer>
 	<a href="JsOS.php">JsOS</a>  <?php if(!isLoggedIn()) echo' |<a href="login.php">Login</a>'?> <?php if(!isLoggedIn()) echo '|<a href="register.php">Register</a>' ?>| <a href="about.php">About</a> | <a href="index.php">Home</a> | <a href="disclaimer.php">Disclaimer</a> <span> | JsOS © All Rights Reserved
